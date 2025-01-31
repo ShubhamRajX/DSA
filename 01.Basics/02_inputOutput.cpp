4475c9e84f38972701943e2bb96beb048b9135cd
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-01-31 23:48:10 +0530

    Add C++ program for input and output of two integers

---
+++ 01.Basics/02_inputOutput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+// #include<bits/stdc++.h>    // It include all the libraries.And This is a non-standard header file and may not be available in all compilers.
+using namespace std;
+
+/*
+int main()
+{
+    int x;
+    cin >> x;
+
+    cout << "The value of x is: " << x;
+    return 0;
+}
+*/
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    cout << "The value of x is: " << x <<" and y is: "<< y;
+    return 0;
+}
