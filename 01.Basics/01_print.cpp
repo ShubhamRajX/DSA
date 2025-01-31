57e068516a3efae5a288c18228b4b13d77a45b4f
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-01-31 23:42:16 +0530

    Add basic C++ program to print names

---
+++ 01.Basics/01_print.cpp
@@ -0,0 +1,30 @@
+// Generic Skleton for C++ programs
+/*
+#include<iostream>
+
+int main()
+{
+    return 0;
+}
+*/
+
+/*
+#include<iostream>
+
+int main()
+{
+    std::cout << "Shubham" << std::endl;
+    std::cout << "Raj" ,"\n";
+    return 0;
+}
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Shubham" << endl;
+    cout << "Raj";
+    return 0;
+}
