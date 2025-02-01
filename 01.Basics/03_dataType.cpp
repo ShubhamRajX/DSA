a9b3103da69227656c7c1743d1487e58d0ad7c39
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-02-02 00:12:43 +0530

    differentDataype

---
+++ 01.Basics/03_dataType.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int
+    /*
+    int x = 4;
+    long y = 50;
+    long long z = 40000000000;
+
+    cout <<y;
+    */
+
+    // float and double
+    /*
+    float a = 4.5;
+    float b = 5;
+    
+    double c = 45.5;
+    double d = 51;
+
+    // cout << b;
+    cout << d;
+    */
+
+    // string and getline
+    /*
+    // string s1, s2; 
+    string s1;      // it take only one word before space for after space use another string
+    string s2;
+    cin >> s1 >>s2;
+    cout << s1 << " " << s2 << endl;
+
+    string s3;
+    getline(cin, s3);  // it take whole line not the next line
+    cout << s3 << endl;
+    */
+
+    // char
+    char ch = 'A';
+    cout << ch << endl;
+
+}
