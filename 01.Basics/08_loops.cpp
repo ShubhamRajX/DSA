67ef0332f57976184fb8e6bf0123ebf96830d2d5
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-02-05 00:17:48 +0530

    forWhileDowhile

---
+++ 01.Basics/08_loops.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // For Loop
+
+    for(int i=1; i<=5; i++)
+    {
+        // cout<<"Shubham "<<i<<endl;
+    }
+    cout<<endl;
+
+    for(int i=5; i>0; i--)
+    {
+        // cout<<"Shubham "<<i<<endl;
+    }
+
+    // While Loop
+
+    int i = 1;
+    while(i <= 5){
+        // cout<<"Raj "<<i <<endl;
+
+        i = i + 1;
+    }
+
+    // Do-While Loop
+
+    i = 1;
+    do{
+        cout<<"Raj "<<i <<endl;
+        i++;
+    }
+    while(i <= 5);
+    
+}
