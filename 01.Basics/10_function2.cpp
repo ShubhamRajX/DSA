1cd94a67ee33149e37d655a345874166ba41ca89
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-02-05 01:01:26 +0530

    Add function to calculate and print the sum of two integers

---
+++ 01.Basics/10_function2.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//take two number and print its sum
+
+/*
+int printSum(int a, int b){
+    // int c = a+b;
+    // cout << c << endl;
+
+    cout<<a+b<<endl;
+   
+}
+
+int main(){
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    int sum = printSum(a,b);
+
+    return 0;
+}
+*/
+
+
+int printSum(int a, int b){
+    // int c = a+b;
+    // return c;
+
+    return a+b;
+}
+
+int main(){
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    // int sum = printSum(a,b);
+    // cout<<sum<<endl;
+
+    int printSum(int a,int b);
+    cout<<printSum(a,b)<<endl;
+    
+    return 0;
+}
+
+
+/*
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cin>>num1>>num2;
+
+    int minimum = min(num1,num2);
+    cout<< minimum <<endl;
+
+    int maximum = max(num1,num2);
+    cout<< maximum <<endl;
+
+    return 0;
+}
+*/
