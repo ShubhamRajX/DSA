c42d3d28bbfb48f62267360e669a7c01eec0ee2e
ShubhamRajX <shubhamraj.1517@gmail.com>
2025-02-04 23:45:14 +0530

    basic

---
+++ 01.Basics/07_arrayString.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // ARRAY
+    // // 1D
+    // int arr[5];
+    // cin>>arr[2];
+    // cout<<arr[2]<<endl;
+    
+    // // 2D
+    // int arr[3][3];
+    // cin>>arr[1][2];
+    // cout<<arr[1][2]<<endl;
+
+    // STRING
+    string str;
+    cin>>str;
+    cout<<str[4]<<endl;
+    cout<<str.size()<<endl;
+    // cout<<str.length()<<endl;
+}
